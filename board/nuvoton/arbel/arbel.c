54c86738ffa480a664f6aa031444890b87ea25bf
Avi Fishman <avifishman70@gmail.com>
2021-08-09 12:12:49 +0300

    board: novoton: arbel: clean warnings
    Signed-off-by: Avi Fishman <avifishman70@gmail.com>

--- board/nuvoton/arbel/arbel.c
+++ board/nuvoton/arbel/arbel.c
@@ -664,10 +664,10 @@ int dram_init(void)
     struct clk_ctl *clkctl = (struct clk_ctl *)(uintptr_t)npcm850_get_base_clk();
 #endif
 
-	int RAMsize = (readl(&gcr->intcr4) >> 20) & 0x7;   /* Read only 3bit's MSB of GMMAP0 */
-
 // For bootblock 0.0.5 and below:
 #if 0
+	int RAMsize = (readl(&gcr->intcr4) >> 20) & 0x7;   /* Read only 3bit's MSB of GMMAP0 */
+
 	switch(RAMsize)
 	{
 		case 1:
@@ -785,6 +785,7 @@ int checkboard(void)
 #endif
 
 #ifdef CONFIG_LAST_STAGE_INIT
+#ifdef SECURE_BOOT
 static bool is_security_enabled(void)
 {
 	struct npcm850_gcr *gcr = (struct npcm850_gcr *)(uintptr_t)npcm850_get_base_gcr();
@@ -912,10 +913,10 @@ static int secure_boot_configuration(void)
 
 	return 0;
 }
+#endif
 
 int last_stage_init(void)
 {
-	int rc;
 	char value[32];
 	struct udevice *dev = gd->cur_serial_dev;
 
@@ -936,16 +937,14 @@ int last_stage_init(void)
 		env_set("console", value);
 
 	}
-#if 0
+#ifdef SECURE_BOOT
 	if (is_security_enabled()) {
-
-		rc = check_nist_version();
+		int rc = check_nist_version();
 		if (rc != 0)
 			return rc;
 	} else {
-
 		// OTP can be programmed only in basic mode
-		rc = secure_boot_configuration();
+		int rc = secure_boot_configuration();
 		if (rc != 0)
 			return rc;
 	}
