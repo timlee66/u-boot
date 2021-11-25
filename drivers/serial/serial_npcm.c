8f02af9e0438ff7a89a6b4d439f457c4df913f0c
Stanley Chu <yschu@nuvoton.com>
2021-11-25 16:23:38 +0800

    serial: npcm: simplify driver and fix coding style
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/serial/serial_npcm.c
+++ drivers/serial/serial_npcm.c
@@ -1,46 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (c) 2016 Nuvoton Technology Corp.
- *
- * SPDX-License-Identifier:	GPL-2.0+
+ * Copyright (c) 2021 Nuvoton Technology.
  */
 
 #include <common.h>
 #include <dm.h>
-#include <errno.h>
-#include <fdtdec.h>
-#include <linux/compiler.h>
-#include <asm/io.h>
 #include <serial.h>
-#include <clk.h>
 #include <asm/arch/uart.h>
 
-DECLARE_GLOBAL_DATA_PTR;
-
-/* Information about a serial port */
-struct npcm_serial_platdata {
-	struct npcm_uart *reg;  /* address of registers in physical memory */
-	u8 port_id;     /* uart port number */
+struct npcm_serial_plat {
+	struct npcm_uart *reg;
 	u32 uart_clk;
 };
 
-int npcm_serial_init(struct npcm_uart *uart)
+static int npcm_serial_init(struct npcm_uart *uart)
 {
 	u8 val;
 
-	/*
-	* Disable all UART interrupt
-	*/
+	/* Disable all UART interrupt */
 	writeb(0, &uart->ier);
 
-	/*
-	* Set port for 8 bit, 1 stop, no parity
-	*/
+	/* Set port for 8 bit, 1 stop, no parity */
 	val = LCR_WLS_8b;
 	writeb(val, &uart->lcr);
 
-	/*
-	* Set the RX FIFO trigger level, reset RX, TX FIFO
-	*/
+	/* Set the RX FIFO trigger level, reset RX, TX FIFO */
 	val = FCR_FME | FCR_RFR | FCR_TFR | FCR_RFITL_4B;
 	writeb(val, &uart->fcr);
 
@@ -49,21 +33,24 @@ int npcm_serial_init(struct npcm_uart *uart)
 
 static int npcm_serial_pending(struct udevice *dev, bool input)
 {
-	struct npcm_serial_platdata *plat = dev->plat_;
+	struct npcm_serial_plat *plat = dev_get_plat(dev);
 	struct npcm_uart *const uart = plat->reg;
 
 	if (input)
 		return (readb(&uart->lsr) & LSR_RFDR);
 	else
 		return !(readb(&uart->lsr) & LSR_THRE);
+
+	return 0;
 }
 
 static int npcm_serial_putc(struct udevice *dev, const char ch)
 {
-	struct npcm_serial_platdata *plat = dev->plat_;
+	struct npcm_serial_plat *plat = dev_get_plat(dev);
 	struct npcm_uart *const uart = plat->reg;
 
-	while (!(readl(&uart->lsr) & LSR_THRE));
+	while (!(readl(&uart->lsr) & LSR_THRE))
+		;
 
 	writeb(ch, &uart->thr);
 
@@ -72,46 +59,24 @@ static int npcm_serial_putc(struct udevice *dev, const char ch)
 
 static int npcm_serial_getc(struct udevice *dev)
 {
-	struct npcm_serial_platdata *plat = dev->plat_;
+	struct npcm_serial_plat *plat = dev_get_plat(dev);
 	struct npcm_uart *const uart = plat->reg;
 
-	while (!(readl(&uart->lsr) & LSR_RFDR));
+	while (!(readl(&uart->lsr) & LSR_RFDR))
+		;
 
 	return (int)(readb(&uart->rbr) & 0xff);
 }
 
 static int npcm_serial_setbrg(struct udevice *dev, int baudrate)
 {
-	struct npcm_serial_platdata *plat = dev->plat_;
+	struct npcm_serial_plat *plat = dev_get_plat(dev);
 	struct npcm_uart *const uart = plat->reg;
 	int ret = 0;
-	s32 divisor;
-	u32 uart_clock;
-
-#ifdef CONFIG_TARGET_ARBEL_PALLADIUM
-	uart_clock = plat->uart_clk;
+	u32 divisor;
 
 	/* BaudOut = UART Clock  / (16 * [Divisor + 2]) */
-	divisor = ((s32)uart_clock / ((s32)baudrate * 16)) - 2;
-
-	divisor = 0;    /* Maximum Baudrate possible  500000000/2/10/32/1000 = 781  */
-#else
-	/* 24MHz = 960MHz(PLL2) / 2 / (19 + 1) */
-	uart_clock = plat->uart_clk;
-
-	/* BaudOut = UART Clock  / (16 * [Divisor + 2]) */
-	divisor = ((s32)uart_clock / ((s32)baudrate * 16)) - 2;
-
-	/* since divisor is rounded down check
-	   if it is better when rounded up */
-	if (((s32)uart_clock / (16 * (divisor + 2)) - baudrate) >
-		(baudrate - (s32)uart_clock / (16 * ((divisor + 1) + 2)))) {
-		divisor++;
-	}
-
-	if (divisor < 0)
-		return -1;
-#endif
+	divisor = DIV_ROUND_CLOSEST(plat->uart_clk, 16 * baudrate + 2) - 2;
 
 	writeb(readb(&uart->lcr) | LCR_DLAB, &uart->lcr);
 	writeb(divisor & 0xff, &uart->dll);
@@ -123,51 +88,13 @@ static int npcm_serial_setbrg(struct udevice *dev, int baudrate)
 
 static int npcm_serial_probe(struct udevice *dev)
 {
-	struct npcm_serial_platdata *plat = dev->plat_;
+	struct npcm_serial_plat *plat = dev_get_plat(dev);
 	struct npcm_uart *const uart = plat->reg;
-	uint clkd[2]; /* clk_id and clk_no, UART clk_no is 1. */
-	struct clk clk;
-	int ret;
 
+	plat->reg = (struct npcm_uart *)dev_read_addr_ptr(dev);
+	plat->uart_clk = dev_read_u32_default(dev, "clock-frequency", 0);
 	npcm_serial_init(uart);
 
-	plat->uart_clk = fdtdec_get_uint(gd->fdt_blob, dev_of_offset(dev),
-					"clock-frequency", 115200);
-
-	ret = fdtdec_get_int_array(gd->fdt_blob, dev_of_offset(dev),
-					"clocks", clkd, 2);
-	if (ret)
-		return ret;
-
-	ret = clk_get_by_index(dev, 0, &clk);
-	if (ret < 0) {
-		printf("Cannot find clk driver!\n");
-		return ret;
-	}
-
-	clk.id = clkd[1];
-
-	/* To set UART clock source and divider */
-	ret = clk_set_rate(&clk, plat->uart_clk);
-	clk_free(&clk);
-	if (ret < 0)
-		return ret;
-
-	return 0;
-}
-
-static int npcm_serial_ofdata_to_platdata(struct udevice *dev)
-{
-	struct npcm_serial_platdata *plat = dev->plat_;
-	void *addr;
-
-	addr = dev_read_addr_ptr(dev);
-	if (addr == NULL)
-		return -EINVAL;
-
-	plat->reg = (struct npcm_uart *)addr;
-	plat->port_id = fdtdec_get_int(gd->fdt_blob, dev_of_offset(dev),
-					"id", dev->seq_);
 	return 0;
 }
 
@@ -188,8 +115,7 @@ U_BOOT_DRIVER(serial_npcm) = {
 	.name	= "serial_npcm",
 	.id	= UCLASS_SERIAL,
 	.of_match = npcm_serial_ids,
-	.of_to_plat = npcm_serial_ofdata_to_platdata,
-	.plat_auto  = sizeof(struct npcm_serial_platdata),
+	.plat_auto  = sizeof(struct npcm_serial_plat),
 	.probe = npcm_serial_probe,
 	.ops	= &npcm_serial_ops,
 	.flags = DM_FLAG_PRE_RELOC,
