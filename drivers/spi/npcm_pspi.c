2da002f8d1dcc58e1291a8b922fb6fe9ae44ede9
Stanley Chu <yschu@nuvoton.com>
2021-11-29 14:48:48 +0800

    spi: npcm: simplify driver and fix coding style
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/spi/npcm_pspi.c
+++ drivers/spi/npcm_pspi.c
@@ -1,170 +1,128 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- *  Copyright (c) 2017 Nuvoton Technology Corp.
- *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * Copyright (c) 2021 Nuvoton Technology.
  */
 
 #include <common.h>
 #include <dm.h>
 #include <spi.h>
 #include <clk.h>
-#include <asm/io.h>
-#include <asm/arch/pspi.h>
 #include <asm/gpio.h>
 #include <linux/iopoll.h>
 
-DECLARE_GLOBAL_DATA_PTR;
-
 #define MAX_DIV	127
-struct npcm_pspi_platdata {
-	phys_addr_t regs;
-	u32 max_hz;
-	u32 dev_num;
-	struct gpio_desc cs_gpio;
-};
+
+/* Register offsets */
+#define PSPI_DATA		0
+#define PSPI_CTL1		2
+#define PSPI_STAT		4
+
+/* PSPI_CTL1 fields */
+#define PSPI_CTL1_SPIEN		BIT(0)
+#define PSPI_CTL1_SCM		BIT(7)
+#define PSPI_CTL1_SCIDL		BIT(8)
+#define PSPI_CTL1_SCDV_MASK	GENMASK(15, 9)
+#define PSPI_CTL1_SCDV_SHIFT	9
+
+/* PSPI_STAT fields */
+#define PSPI_STAT_BSY		BIT(0)
+#define PSPI_STAT_RBF		BIT(1)
 
 struct npcm_pspi_priv {
-	struct npcm_pspi_regs *pspi_regs;
-	struct clk pspi_clk;
-	u32 cs;
-	enum pspi_dev pspi_dev_num;
+	void __iomem *base;
+	struct clk clk;
+	struct gpio_desc cs_gpio;
+	u32 max_hz;
 };
 
-static void dump_regs(struct npcm_pspi_priv *priv)
-{
-	struct npcm_pspi_regs *regs = priv->pspi_regs;
-
-	printf("pspi_stat=0x%02x\n", readb(&regs->pspi_stat));
-	printf("pspi_ctl1=0x%04x\n", readw(&regs->pspi_ctl1));
-}
-
-static void spi_cs_activate(struct udevice *dev)
-{
-	struct udevice *bus = dev->parent;
-	struct npcm_pspi_platdata *plat = dev_get_plat(bus);
-
-	dm_gpio_set_value(&plat->cs_gpio, 0);
-
-	return;
-}
-
-static void spi_cs_deactivate(struct udevice *dev)
-{
-	struct udevice *bus = dev->parent;
-	struct npcm_pspi_platdata *plat = dev_get_plat(bus);
-
-	dm_gpio_set_value(&plat->cs_gpio, 1);
-
-	return;
-}
-
-
-static int npcm_pspi_claim_bus(struct udevice *dev)
-{
-	return 0;
-}
-
-static int npcm_pspi_release_bus(struct udevice *dev)
-{
-	return 0;
-}
-
-static int npcm_pspi_xfer(struct udevice *dev, unsigned int bitlen,
-			const void *dout, void *din, unsigned long flags)
+static inline void spi_cs_activate(struct udevice *dev)
 {
 	struct udevice *bus = dev->parent;
 	struct npcm_pspi_priv *priv = dev_get_priv(bus);
-	struct npcm_pspi_regs *regs = priv->pspi_regs;
-	unsigned int bytes = bitlen / 8;
-	const unsigned char *tx = dout;
-	unsigned char *rx = din;
-	char tmp;
+
+	dm_gpio_set_value(&priv->cs_gpio, 0);
+}
+
+static inline void spi_cs_deactivate(struct udevice *dev)
+{
+	struct udevice *bus = dev->parent;
+	struct npcm_pspi_priv *priv = dev_get_priv(bus);
+
+	dm_gpio_set_value(&priv->cs_gpio, 1);
+}
+
+static inline void npcm_pspi_enable(struct npcm_pspi_priv *priv)
+{
+	u16 val;
+
+	val = readw(priv->base + PSPI_CTL1);
+	val |= PSPI_CTL1_SPIEN;
+	writew(val, priv->base + PSPI_CTL1);
+}
+
+static inline void npcm_pspi_disable(struct npcm_pspi_priv *priv)
+{
+	u16 val;
+
+	val = readw(priv->base + PSPI_CTL1);
+	val &= ~PSPI_CTL1_SPIEN;
+	writew(val, priv->base + PSPI_CTL1);
+}
+
+static int npcm_pspi_xfer(struct udevice *dev, unsigned int bitlen,
+			  const void *dout, void *din, unsigned long flags)
+{
+	struct udevice *bus = dev->parent;
+	struct npcm_pspi_priv *priv = dev_get_priv(bus);
+	void __iomem *base = priv->base;
+	const u8 *tx = dout;
+	u8 *rx = din;
+	u32 bytes = bitlen / 8;
+	u8 tmp;
 	u32 val;
 	int i, ret = 0;
 
-	/* Cleaning junk data in the buffer */
-	if (readb(&regs->pspi_stat) & (0x1 << PSPI_STAT_RBF))
-		readb(&regs->pspi_data);
+	npcm_pspi_enable(priv);
 
 	if (flags & SPI_XFER_BEGIN)
-		/* Setting chip select low to start transaction */
 		spi_cs_activate(dev);
 
-	/* Writing and reading the data */
 	for (i = 0; i < bytes; i++) {
-
 		/* Making sure we can write */
-		ret = readb_poll_timeout(&regs->pspi_stat, val,
-				!(val & (0x1 << PSPI_STAT_BSY)), 1000000);
-		if (ret == -ETIMEDOUT) {
-			printf("%s: state is busy\n", __func__);
-			dump_regs(priv);
-			ret = -EBUSY;
-			goto err;
-		}
+		ret = readb_poll_timeout(base + PSPI_STAT, val,
+					 !(val & PSPI_STAT_BSY),
+					 1000000);
+		if (ret < 0)
+			break;
 
 		if (tx)
-			writeb(*tx++, &regs->pspi_data);
+			writeb(*tx++, base + PSPI_DATA);
 		else
-			writeb(0, &regs->pspi_data);
+			writeb(0, base + PSPI_DATA);
 
 		/* Wait till write completed */
-		ret = readb_poll_timeout(&regs->pspi_stat, val,
-				!(val & (0x1 << PSPI_STAT_BSY)), 1000000);
-		if (ret == -ETIMEDOUT) {
-			printf("%s: state is busy after write\n", __func__);
-			dump_regs(priv);
-			ret = -EBUSY;
-			goto err;
-		}
+		ret = readb_poll_timeout(base + PSPI_STAT, val,
+					 !(val & PSPI_STAT_BSY),
+					 1000000);
+		if (ret < 0)
+			break;
 
-		/* Waiting till reading is finished*/
-		ret = readb_poll_timeout(&regs->pspi_stat, val,
-				(val & (0x1 << PSPI_STAT_RBF)), 1000000);
-		if (ret == -ETIMEDOUT) {
-			printf("%s: read buf is empty\n", __func__);
-			dump_regs(priv);
-			ret = -EBUSY;
-			goto err;
-		}
+		/* Wait till read buffer full */
+		ret = readb_poll_timeout(base + PSPI_STAT, val,
+					 (val & PSPI_STAT_RBF),
+					 1000000);
+		if (ret < 0)
+			break;
 
-		tmp = readb(&regs->pspi_data);
+		tmp = readb(base + PSPI_DATA);
 		if (rx)
 			*rx++ = tmp;
 	}
 
-	if (flags & SPI_XFER_END) {
-		ret = readb_poll_timeout(&regs->pspi_stat, val,
-				!(val & (0x1 << PSPI_STAT_BSY)), 1000000);
-		if (ret == -ETIMEDOUT) {
-			printf("%s: state is busy after xfer\n", __func__);
-			dump_regs(priv);
-			ret = -EBUSY;
-		}
-
+	if (flags & SPI_XFER_END)
 		spi_cs_deactivate(dev);
-	}
 
-	return ret;
-
-err:
-	spi_cs_deactivate(dev);
+	npcm_pspi_disable(priv);
 
 	return ret;
 }
@@ -172,42 +130,28 @@ err:
 static int npcm_pspi_set_speed(struct udevice *bus, uint speed)
 {
 	struct npcm_pspi_priv *priv = dev_get_priv(bus);
-	struct npcm_pspi_platdata *plat = dev_get_plat(bus);
+	ulong apb_clock;
 	u32 divisor;
-	u32 apb_clock;
+	u16 val;
 
-	apb_clock = clk_get_rate(&priv->pspi_clk);
+	apb_clock = clk_get_rate(&priv->clk);
 	if (!apb_clock)
 		return -EINVAL;
 
-	if (speed > plat->max_hz)
-		speed = plat->max_hz;
+	if (speed > priv->max_hz)
+		speed = priv->max_hz;
 
-	/* Disabling the module for configuration */
-	writew(readw(&priv->pspi_regs->pspi_ctl1) & ~(0x1 << PSPI_CTL1_SPIEN),
-		&priv->pspi_regs->pspi_ctl1);
-
-	/* Calculating divisor */
 	divisor = DIV_ROUND_CLOSEST(apb_clock, (2 * speed) - 1);
 	if (divisor > MAX_DIV)
 		divisor = MAX_DIV;
 
-	debug("%s: apb_clock=%u speed=%d divisor=%u\n",
-			__func__,	apb_clock, speed, divisor);
+	val = readw(priv->base + PSPI_CTL1);
+	val &= ~PSPI_CTL1_SCDV_MASK;
+	val |= divisor << PSPI_CTL1_SCDV_SHIFT;
+	writew(val, priv->base + PSPI_CTL1);
 
-	/* If requested clock frequency is to big we return ERROR */
-	if (divisor <= 0)
-		return -EINVAL;
-
-	/* Setting the divisor */
-	writew((readw(&priv->pspi_regs->pspi_ctl1) &
-				~(0x7f << PSPI_CTL1_SCDV6_0)) |
-			(divisor << PSPI_CTL1_SCDV6_0),
-			&priv->pspi_regs->pspi_ctl1);
-
-	/* Enabling the PSPI module, 8-bit,  */
-	writew(readw(&priv->pspi_regs->pspi_ctl1) | (0x1 << PSPI_CTL1_SPIEN),
-			&priv->pspi_regs->pspi_ctl1);
+	debug("%s: apb_clock=%lu speed=%d divisor=%u\n",
+	      __func__, apb_clock, speed, divisor);
 
 	return 0;
 }
@@ -215,88 +159,60 @@ static int npcm_pspi_set_speed(struct udevice *bus, uint speed)
 static int npcm_pspi_set_mode(struct udevice *bus, uint mode)
 {
 	struct npcm_pspi_priv *priv = dev_get_priv(bus);
-	unsigned short pspi_mode;
-
-	/* Disabling the module for configuration */
-	writew(readw(&priv->pspi_regs->pspi_ctl1) & ~(0x1 << PSPI_CTL1_SPIEN),
-			&priv->pspi_regs->pspi_ctl1);
+	u16 pspi_mode, val;
 
 	switch (mode & (SPI_CPOL | SPI_CPHA)) {
 	case SPI_MODE_0:
 		pspi_mode = 0;
 		break;
 	case SPI_MODE_1:
-		pspi_mode = (0x1 << PSPI_CTL1_SCIDL);
+		pspi_mode = PSPI_CTL1_SCM;
 		break;
 	case SPI_MODE_2:
-		pspi_mode = (0x1 << PSPI_CTL1_SCM);
+		pspi_mode = PSPI_CTL1_SCIDL;
 		break;
 	case SPI_MODE_3:
-		pspi_mode = (0x1 << PSPI_CTL1_SCIDL) | (0x1 << PSPI_CTL1_SCM);
+		pspi_mode = PSPI_CTL1_SCIDL | PSPI_CTL1_SCM;
 		break;
 	default:
 		break;
 	}
 
-	/* Set Mode and Enable the PSPI module */
-	writew(readw(&priv->pspi_regs->pspi_ctl1) | (0x1 << PSPI_CTL1_SPIEN) | (pspi_mode),
-			&priv->pspi_regs->pspi_ctl1);
-
-	return 0;
-}
-
-static int npcm_spi_ofdata_to_platdata(struct udevice *bus)
-{
-	struct npcm_pspi_platdata *plat = dev_get_plat(bus);
-	const void *blob = gd->fdt_blob;
-	int node = dev_of_offset(bus);
-
-	plat->regs = (phys_addr_t)dev_read_addr_ptr(bus);
-	plat->max_hz = fdtdec_get_int(blob, node, "spi-max-frequency",
-			500000);
-
-	plat->dev_num = fdtdec_get_int(blob, node, "index", 0);
+	val = readw(priv->base + PSPI_CTL1);
+	val &= ~(PSPI_CTL1_SCIDL | PSPI_CTL1_SCM);
+	val |= pspi_mode;
+	writew(val, priv->base + PSPI_CTL1);
 
 	return 0;
 }
 
 static int npcm_pspi_probe(struct udevice *bus)
 {
-	struct npcm_pspi_platdata *plat = dev_get_plat(bus);
 	struct npcm_pspi_priv *priv = dev_get_priv(bus);
 	int node = dev_of_offset(bus);
 	int ret;
 
-	ret = clk_get_by_index(bus, 0, &priv->pspi_clk);
-	if (ret < 0) {
-		printf("%s:Probe failed: Failed to get clk!\n", __func__);
+	ret = clk_get_by_index(bus, 0, &priv->clk);
+	if (ret < 0)
 		return ret;
-	}
 
-	priv->pspi_dev_num = (enum pspi_dev)plat->dev_num;
-	priv->pspi_regs = (struct npcm_pspi_regs *)plat->regs;
-
+	priv->base = dev_read_addr_ptr(bus);
+	priv->max_hz = dev_read_u32_default(bus, "spi-max-frequency", 0);
 	gpio_request_by_name_nodev(offset_to_ofnode(node), "cs-gpios", 0,
-				&plat->cs_gpio, GPIOD_IS_OUT);
+				   &priv->cs_gpio, GPIOD_IS_OUT);
 
 	return 0;
 }
 
-
 static const struct dm_spi_ops npcm_pspi_ops = {
-	.claim_bus      = npcm_pspi_claim_bus,
-	.release_bus    = npcm_pspi_release_bus,
 	.xfer           = npcm_pspi_xfer,
 	.set_speed      = npcm_pspi_set_speed,
 	.set_mode       = npcm_pspi_set_mode,
-	/*
-	 * cs_info is not needed, since we require all chip selects to be
-	 * in the device tree explicitly
-	 */
 };
 
 static const struct udevice_id npcm_pspi_ids[] = {
 	{ .compatible = "nuvoton,npcm845-pspi"},
+	{ .compatible = "nuvoton,npcm750-pspi"},
 	{ }
 };
 
@@ -305,8 +221,6 @@ U_BOOT_DRIVER(npcm_pspi) = {
 	.id     = UCLASS_SPI,
 	.of_match = npcm_pspi_ids,
 	.ops    = &npcm_pspi_ops,
-	.of_to_plat = npcm_spi_ofdata_to_platdata,
-	.plat_auto = sizeof(struct npcm_pspi_platdata),
 	.priv_auto = sizeof(struct npcm_pspi_priv),
 	.probe  = npcm_pspi_probe,
 };
