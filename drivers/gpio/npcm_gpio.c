1ff89f4f5d9cb209bff20e52c95fff8c840ef801
Stanley Chu <yschu@nuvoton.com>
2021-12-01 11:16:53 +0800

    gpio: npcm: fix coding style
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/gpio/npcm_gpio.c
+++ drivers/gpio/npcm_gpio.c
@@ -1,8 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2016 Dell Inc.
- * Eugene Cho  <eugene.cho@dell.com>
- *
- * SPDX-License-Identifier:	GPL-2.0+
+ * Copyright (c) 2021 Nuvoton Technology.
  */
 
 #include <common.h>
@@ -17,19 +15,18 @@
 #define NPCM_GPIO_PORTS_PER_BANK    32
 
 #define NPCM_GPIO_REG_DIN		0x04	/* RO - Data In */
-#define NPCM_GPIO_REG_DOUT		0x0C	/* RW - Data Out Re gister */
+#define NPCM_GPIO_REG_DOUT		0x0C	/* RW - Data Out */
 #define NPCM_GPIO_REG_IEM		0x58	/* RW - Input Enable Mask */
-#define NPCM_GPIO_REG_OE		0x10	/* RW - Output Enable Register */
+#define NPCM_GPIO_REG_OE		0x10	/* RW - Output Enable */
 #define NPCM_GPIO_REG_OES		0x70	/* WO - Output Enable Register Set */
 #define NPCM_GPIO_REG_OEC		0x74	/* WO - Output Enable Register Clear */
 
 struct npcm_gpio_priv {
 	void __iomem *base;
-	char bank_name[16];
 };
 
-static void npcm_gpio_offset_write(struct udevice *dev, unsigned offset,
-				       unsigned reg, int value)
+static void npcm_gpio_offset_write(struct udevice *dev, unsigned int offset,
+				   unsigned int reg, int value)
 {
 	struct npcm_gpio_priv *priv = dev_get_priv(dev);
 	u32 tmp;
@@ -44,55 +41,57 @@ static void npcm_gpio_offset_write(struct udevice *dev, unsigned offset,
 	writel(tmp, priv->base + reg);
 }
 
-static int npcm_gpio_offset_read(struct udevice *dev, unsigned offset,
-				     unsigned reg)
+static int npcm_gpio_offset_read(struct udevice *dev, unsigned int offset,
+				 unsigned int reg)
 {
 	struct npcm_gpio_priv *priv = dev_get_priv(dev);
 
 	return !!(readl(priv->base + reg) & BIT(offset));
 }
 
-static int npcm_gpio_direction_input(struct udevice *dev, unsigned offset)
+static int npcm_gpio_direction_input(struct udevice *dev, unsigned int offset)
 {
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_OEC, 1);
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_IEM, 1);
+
 	return 0;
 }
 
-static int npcm_gpio_direction_output(struct udevice *dev, unsigned offset,
-					  int value)
+static int npcm_gpio_direction_output(struct udevice *dev, unsigned int offset,
+				      int value)
 {
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_IEM, 0);
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_OES, 1);
-
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_DOUT, value);
+
 	return 0;
 }
 
-static int npcm_gpio_get_value(struct udevice *dev, unsigned offset)
+static int npcm_gpio_get_value(struct udevice *dev, unsigned int offset)
 {
-	if(npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_IEM))
+	if (npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_IEM))
 		return npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_DIN);
 
-	if(npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_OE))
+	if (npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_OE))
 		return npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_DOUT);
 
-	return -1;
+	return -EINVAL;
 }
 
-static int npcm_gpio_set_value(struct udevice *dev, unsigned offset,
-				   int value)
+static int npcm_gpio_set_value(struct udevice *dev, unsigned int offset,
+			       int value)
 {
 	npcm_gpio_offset_write(dev, offset, NPCM_GPIO_REG_DOUT, value);
+
 	return 0;
 }
 
-static int npcm_gpio_get_function(struct udevice *dev, unsigned offset)
+static int npcm_gpio_get_function(struct udevice *dev, unsigned int offset)
 {
-	if(npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_IEM))
+	if (npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_IEM))
 		return GPIOF_INPUT;
 
-	if(npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_OE))
+	if (npcm_gpio_offset_read(dev, offset, NPCM_GPIO_REG_OE))
 		return GPIOF_OUTPUT;
 
 	return GPIOF_FUNC;
@@ -110,29 +109,18 @@ static int npcm_gpio_probe(struct udevice *dev)
 {
 	struct npcm_gpio_priv *priv = dev_get_priv(dev);
 	struct gpio_dev_priv *uc_priv = dev_get_uclass_priv(dev);
-	fdt_addr_t addr;
-	unsigned int tmp;
 
-	addr = dev_read_addr(dev);
-	if (addr == FDT_ADDR_T_NONE)
-		return -EINVAL;
-
-	priv->base = (void __iomem *)addr;
+	priv->base = dev_read_addr_ptr(dev);
 	uc_priv->gpio_count = NPCM_GPIO_PORTS_PER_BANK;
+	uc_priv->bank_name = dev_read_string(dev, "gpio-bank-name");
 
-	tmp = (addr >> 12) & 0x7;
-
-	snprintf(priv->bank_name, sizeof(priv->bank_name) - 1,
-		 "bank%d-", tmp);
-
-	uc_priv->bank_name = priv->bank_name;
 	return 0;
 }
 
-/* .data = the number of GPIO banks */
 static const struct udevice_id npcm_gpio_match[] = {
 	{ .compatible = "nuvoton,npcm845-gpio" },
-	{ /* sentinel */ }
+	{ .compatible = "nuvoton,npcm750-gpio" },
+	{ }
 };
 
 U_BOOT_DRIVER(npcm_gpio) = {
