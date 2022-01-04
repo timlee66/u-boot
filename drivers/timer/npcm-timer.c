f9c2b285a3779f711e010734f4aa93700ba4d52c
Stanley Chu <yschu@nuvoton.com>
2022-01-04 14:06:13 +0800

    timer: npcm: Add support for NPCM750
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/timer/npcm-timer.c
+++ drivers/timer/npcm-timer.c
@@ -4,12 +4,15 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <dm.h>
 #include <timer.h>
 #include <asm/io.h>
 
-#define NPCM_CLOCK_RATE				1000000
-#define NPCM_TIMER_INIT_VAL			0xFFFFFFFF
+#define NPCM_TIMER_CLOCK_RATE			1000000UL
+#define NPCM_TIMER_INPUT_RATE			25000000UL
+#define NPCM_TIMER_INIT_VAL			0x00FFFFFF
+#define NPCM_TIMER_TDR_MASK			GENMASK(23, 0)
 
 /* Register offsets */
 #define TCR0	0x0
@@ -23,6 +26,8 @@
 
 struct npcm_timer_priv {
 	void __iomem *base;
+	u32 last_count;
+	u64 counter;
 };
 
 static u64 npcm_timer_get_count(struct udevice *dev)
@@ -30,23 +35,41 @@ static u64 npcm_timer_get_count(struct udevice *dev)
 	struct npcm_timer_priv *priv = dev_get_priv(dev);
 	u32 val;
 
-	val = NPCM_TIMER_INIT_VAL - readl(priv->base + TDR0);
+	/* The counter is couting down, inverse the value */
+	val = NPCM_TIMER_INIT_VAL - (readl(priv->base + TDR0) & NPCM_TIMER_TDR_MASK);
 
-	return timer_conv_64(val);
+	if (val >= priv->last_count)
+		priv->counter += (val - priv->last_count);
+	else
+		priv->counter += (NPCM_TIMER_INIT_VAL + 1 - priv->last_count) + val;
+	priv->last_count = val;
+
+	return priv->counter;
 }
 
 static int npcm_timer_probe(struct udevice *dev)
 {
 	struct timer_dev_priv *uc_priv = dev_get_uclass_priv(dev);
 	struct npcm_timer_priv *priv = dev_get_priv(dev);
+	struct clk clk;
+	int ret;
 
 	priv->base = dev_read_addr_ptr(dev);
 	if (!priv->base)
 		return -ENOENT;
-	uc_priv->clock_rate = NPCM_CLOCK_RATE;
+	priv->counter = 0;
+	priv->last_count = 0;
+	uc_priv->clock_rate = NPCM_TIMER_CLOCK_RATE;
 
-	writel(0, priv->base + TCR0);
-	writel(NPCM_TIMER_INIT_VAL, priv->base + TICR0);
+	if (IS_ENABLED(CONFIG_ARCH_NPCM750)) {
+		ret = clk_get_by_index(dev, 0, &clk);
+		if (ret < 0)
+			return ret;
+
+		ret = clk_set_rate(&clk, NPCM_TIMER_INPUT_RATE);
+		if (ret < 0)
+			return ret;
+	}
 
 	/*
 	 * Configure timer and start
@@ -55,6 +78,8 @@ static int npcm_timer_probe(struct udevice *dev)
 	 * prescale = 25
 	 * clock rate = 25Mhz/25 = 1Mhz
 	 */
+	writel(0, priv->base + TCR0);
+	writel(NPCM_TIMER_INIT_VAL, priv->base + TICR0);
 	writel(TCR_EN | TCR_MODE_PERIODIC | TCR_PRESCALE_25,
 	       priv->base + TCR0);
 
