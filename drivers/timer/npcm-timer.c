0f1da07a65f4972c82226b2934366cbee9a44dbc
Stanley Chu <yschu@nuvoton.com>
2022-03-25 09:12:19 +0800

    timer: npcm: sync to upstream version
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/timer/npcm-timer.c
+++ drivers/timer/npcm-timer.c
@@ -24,10 +24,17 @@
 #define TCR_EN			BIT(30)
 #define TCR_PRESCALE		(NPCM_TIMER_INPUT_RATE / NPCM_TIMER_CLOCK_RATE - 1)
 
-/*
- * 24-bits down-counting hw timer.
- * last_count: last hw counter value.
- * counter: the value to be returned for get_count ops.
+enum input_clock_type {
+	INPUT_CLOCK_FIXED,	/* input clock rate is fixed */
+	INPUT_CLOCK_NON_FIXED
+};
+
+/**
+ * struct npcm_timer_priv - private data for npcm timer driver
+ * npcm timer is a 24-bits down-counting timer.
+ *
+ * @last_count: last hw counter value
+ * @counter: the value to be returned for get_count ops
  */
 struct npcm_timer_priv {
 	void __iomem *base;
@@ -40,7 +47,7 @@ static u64 npcm_timer_get_count(struct udevice *dev)
 	struct npcm_timer_priv *priv = dev_get_priv(dev);
 	u32 val;
 
-	/* The timer is couting down */
+	/* The timer is counting down */
 	val = readl(priv->base + TDR0) & NPCM_TIMER_TDR_MASK;
 	if (val <= priv->last_count)
 		priv->counter += priv->last_count - val;
@@ -55,17 +62,16 @@ static int npcm_timer_probe(struct udevice *dev)
 {
 	struct timer_dev_priv *uc_priv = dev_get_uclass_priv(dev);
 	struct npcm_timer_priv *priv = dev_get_priv(dev);
+	enum input_clock_type type = dev_get_driver_data(dev);
 	struct clk clk;
 	int ret;
 
 	priv->base = dev_read_addr_ptr(dev);
 	if (!priv->base)
-		return -ENOENT;
-	priv->counter = 0;
-	priv->last_count = 0;
+		return -EINVAL;
 	uc_priv->clock_rate = NPCM_TIMER_CLOCK_RATE;
 
-	if (IS_ENABLED(CONFIG_ARCH_NPCM7xx)) {
+	if (type == INPUT_CLOCK_NON_FIXED) {
 		ret = clk_get_by_index(dev, 0, &clk);
 		if (ret < 0)
 			return ret;
@@ -78,7 +84,6 @@ static int npcm_timer_probe(struct udevice *dev)
 	/*
 	 * Configure timer and start
 	 * periodic mode
-	 * input clock freq = 25Mhz
 	 * timer clock rate = input clock / prescale
 	 */
 	writel(0, priv->base + TCR0);
@@ -94,8 +99,8 @@ static const struct timer_ops npcm_timer_ops = {
 };
 
 static const struct udevice_id npcm_timer_ids[] = {
-	{ .compatible = "nuvoton,npcm845-timer" },
-	{ .compatible = "nuvoton,npcm750-timer" },
+	{ .compatible = "nuvoton,npcm845-timer", .data = INPUT_CLOCK_FIXED},
+	{ .compatible = "nuvoton,npcm750-timer", .data = INPUT_CLOCK_NON_FIXED},
 	{}
 };
 
