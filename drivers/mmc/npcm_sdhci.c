0dc961866290ce72c03aa0c335b0038578b30085
Jim Liu <JJLIU0@nuvoton.com>
2022-05-13 16:48:14 +0800

    npcm7xx: mmc : modify dts compatible name

--- drivers/mmc/npcm_sdhci.c
+++ drivers/mmc/npcm_sdhci.c
@@ -72,7 +72,7 @@ static int npcm_sdhci_bind(struct udevice *dev)
 }
 
 static const struct udevice_id npcm_mmc_ids[] = {
-	{ .compatible = "nuvoton,npcm750-sdhci-eMMC" },
+	{ .compatible = "nuvoton,npcm750-sdhci" },
 	{ .compatible = "nuvoton,npcm845-sdhci" },
 	{ }
 };
